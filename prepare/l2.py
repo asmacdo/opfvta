67a0f088f708d3655c33824e67ba9bd211149a0e
Horea Christian <horea.christ@yandex.com>
2019-02-11 17:30:27 +0000

    Updated level2 organization

--- prepare/l2.py
+++ prepare/l2.py
@@ -5,11 +5,11 @@ scratch_dir = '~/data_scratch/opfvta'
 l1_base = '{}/l1/'.format(scratch_dir)
 
 glm.l2_common_effect(l1_base,
-	workflow_name='omnibus',
+	workflow_name='l2',
 	mask='/home/hioanas/gentoo/usr/share/mouse-brain-atlases/dsurqec_200micron_mask.nii',
 	groupby='task',
-	keep_work=True,
 	n_jobs_percentage=.33,
-	#include={'run':['1'],},
-	out_base='{}/l2'.format(scratch_dir),
+	#The JPogT task in the current data has only one run, and cannot be modelled at the second level.
+	exclude={'task':['JPogT'],},
+	out_base=scratch_dir,
 	)
