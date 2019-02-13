a0d5d76c98b558feae70eff9ea32453a0815c984
Horea Christian <horea.christ@yandex.com>
2019-02-13 19:57:03 +0100

    Groups assignment file

--- prepare/groups.py
+++ prepare/groups.py
@@ -12,27 +12,25 @@ ids = [animal_id(db_path, 'ETH/AIC', str(i)) for i in subjects]
 genotypes = parameterized(db_path, 'animals info', animal_filter=ids)
 genotypes = genotypes.loc[genotypes['AnimalExternalIdentifier_database']=='ETH/AIC']
 
-operations = animal_operations(db_path, animal_ids=ids)
-_operations = pd.DataFrame([])
-for i in operations['Animal_id'].unique():
-	df = operations.loc[operations['Animal_id']==i]
+_operations = animal_operations(db_path, animal_ids=ids)
+operations = pd.DataFrame([])
+for i in _operations['Animal_id'].unique():
+	df = _operations.loc[_operations['Animal_id']==i]
 	df = df.drop(['Operation_id','Operation_operator_id','Operation_anesthesia_id'], 1)
 	if len(df) == 1:
 		df['Operation_delay'] = 0
-		_operations = pd.concat([_operations,df],sort=True)
+		operations = pd.concat([operations,df],sort=True)
 	else:
 		a,b = df['Operation_date'].unique()
 		delay = (b-a)/np.timedelta64(1, 'D')
 		delay = round(int(delay))
 		df = df.groupby('Animal_id').transform(lambda x: x.bfill().ffill())
 		df['Animal_id'] = i
+		df['Operation_date'] = df['Operation_date'].tolist()[0]
+		df = df.drop_duplicates()
 		df['Operation_delay'] = delay
-		_operations = pd.concat([_operations,df],sort=True)
+		operations = pd.concat([operations,df],sort=True)
 
-#print(ids)
-#print(genotypes['AnimalExternalIdentifier_animal_id'])
-#print(operations['Animal_id'])
-_operations.to_csv('_operations.csv')
-#print(len(operations))
-#print(len(genotypes))
-#print(operations[['OrthogonalStereotacticTarget_code','Virus_OrthogonalStereotacticTarget_code']])
+
+operations['Subject'] = operations['Animal_id'].apply(lambda x: animal_id(db_path, 'ETH/AIC', x, reverse=True))
+operations.to_csv('../data/groups.csv')
