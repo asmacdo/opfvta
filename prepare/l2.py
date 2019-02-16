3e9e740a53f4fbfd96f2c6a472a4552cebc85c20
Horea Christian <horea.christ@yandex.com>
2019-02-16 21:24:21 +0000

    reordered arguments

--- prepare/l2.py
+++ prepare/l2.py
@@ -18,8 +18,8 @@ other_animals = groups.loc[~groups['Subject'].isin(filtered_animals), 'Subject']
 glm.l2_common_effect(l1_base,
 	workflow_name='l2',
 	mask='/home/hioanas/gentoo/usr/share/mouse-brain-atlases/dsurqec_200micron_mask.nii',
-	n_jobs_percentage=.33,
 	groupby='subject_set',
+	n_jobs_percentage=.33,
 	#The JPogT task in the current data has only one run, and cannot be modelled at the second level.
 	exclude={'task':['JPogT'],},
 	out_base=scratch_dir,
