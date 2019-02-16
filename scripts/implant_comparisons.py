db83e6d0273f0e8fb4755d1b04313371a1b0fe2e
Horea Christian <horea.christ@yandex.com>
2019-02-16 10:08:23 +0100

    t-test for coordinates

---
+++ scripts/implant_comparisons.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import matplotlib as mpl
+from os import path
+from scipy import stats
+
+data_path = path.abspath('data/functional_t.csv')
+df = pd.read_csv(data_path)
+
+groups_path = path.abspath('data/groups.csv')
+groups = pd.read_csv(groups_path)
+
+df = pd.merge(df, groups, on='Subject', how='outer')
+
+df['Task Category'] = df['Task']
+task_categories = {
+	'CogBr':'Block',
+	'CogBm':'Block',
+	'CogMwf':'Block',
+	'CogB':'Block',
+	'JPogP':'Phasic',
+	'CogP':'Phasic',
+	'JPogT':'Tonic',
+	}
+df = df.replace({'Task Category': task_categories})
+df = df.loc[df['Task Category'] == 'Block']
+# do not take into account unimplanted animals
+df = df[pd.notnull(df['PA rel. Bregma [mm]'])]
+
+pas = df['PA rel. Bregma [mm]'].unique()
+for pa in pas:
+	values = df.loc[df['PA rel. Bregma [mm]']==pa, 'Mean VTA t'].tolist()
+	p = stats.ttest_1samp(values, 0).pvalue
+	print(pa, p, p*len(pas))
+
+depths = df['Depth rel. skull [mm]'].unique()
+for depth in depths:
+	values = df.loc[df['Depth rel. skull [mm]']==depth, 'Mean VTA t'].tolist()
+	p = stats.ttest_1samp(values, 0).pvalue
+	print(depth, p, p*len(depths))
