e95009b51b9017cf3b4909251073b206f917cc83
Horea Christian <horea.christ@yandex.com>
2019-02-16 16:20:37 +0100

    Implant comparison script

--- scripts/implant_comparisons.py
+++ scripts/implant_comparisons.py
@@ -2,6 +2,7 @@ import pandas as pd
 import matplotlib as mpl
 from os import path
 from scipy import stats
+from itertools import product
 
 data_path = path.abspath('data/functional_t.csv')
 df = pd.read_csv(data_path)
@@ -27,13 +28,19 @@ df = df.loc[df['Task Category'] == 'Block']
 df = df[pd.notnull(df['PA rel. Bregma [mm]'])]
 
 pas = df['PA rel. Bregma [mm]'].unique()
-for pa in pas:
-	values = df.loc[df['PA rel. Bregma [mm]']==pa, 'Mean VTA t'].tolist()
-	p = stats.ttest_1samp(values, 0).pvalue
-	print(pa, p, p*len(pas))
+#for pa in pas:
+#	values = df.loc[df['PA rel. Bregma [mm]']==pa, 'Mean VTA t'].tolist()
+#	p = stats.ttest_1samp(values, 0).pvalue
+#	print(pa, p, p*len(pas))
 
 depths = df['Depth rel. skull [mm]'].unique()
-for depth in depths:
-	values = df.loc[df['Depth rel. skull [mm]']==depth, 'Mean VTA t'].tolist()
+#for depth in depths:
+#	values = df.loc[df['Depth rel. skull [mm]']==depth, 'Mean VTA t'].tolist()
+#	p = stats.ttest_1samp(values, 0).pvalue
+#	print(depth, p, p*len(depths))
+
+for depth, pa in product(depths,pas):
+	my_slice = df.loc[(df['PA rel. Bregma [mm]'] == pa) & (df['Depth rel. skull [mm]'] == depth)]
+	values = my_slice['Mean VTA t'].tolist()
 	p = stats.ttest_1samp(values, 0).pvalue
-	print(depth, p, p*len(depths))
+	print(depth,pa,p)
