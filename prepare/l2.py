45272b36dc7872f6e34942e1d24e0123f0ee2623
Horea Christian <horea.christ@yandex.com>
2019-02-17 04:57:21 +0100

    Attempting 3d plot

--- prepare/l2.py
+++ prepare/l2.py
@@ -17,7 +17,7 @@ other_animals = [str(i) for i in groups.loc[~groups['Subject'].isin(filtered_ani
 
 glm.l2_common_effect(l1_base,
 	workflow_name='l2',
-	mask='/home/hioanas/gentoo/usr/share/mouse-brain-atlases/dsurqec_200micron_mask.nii',
+	mask='/usr/share/mouse-brain-atlases/dsurqec_200micron_mask.nii',
 	groupby='task',
 	n_jobs_percentage=.33,
 	#The JPogT task in the current data has only one run, and cannot be modelled at the second level.
@@ -27,7 +27,7 @@ glm.l2_common_effect(l1_base,
 
 glm.l2_common_effect(l1_base,
 	workflow_name='l2',
-	mask='/home/hioanas/gentoo/usr/share/mouse-brain-atlases/dsurqec_200micron_mask.nii',
+	mask='/usr/share/mouse-brain-atlases/dsurqec_200micron_mask.nii',
 	groupby='subject_set',
 	n_jobs_percentage=.33,
 	#Only considering block stimulation (filtering out tonic and phasic stimulation).
