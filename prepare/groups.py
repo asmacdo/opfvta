9c18a4774416745438739acb891e4dc42dcc4841
Horea Christian <horea.christ@yandex.com>
2019-04-03 03:35:01 +0200

    Updated groups metadata

--- prepare/groups.py
+++ prepare/groups.py
@@ -1,9 +1,11 @@
 from labbookdb.report.tracking import append_external_identifiers
-from labbookdb.report.selection import animals_by_genotype
+from labbookdb.report.selection import animal_id, animals_by_genotype
 
 db_path='~/syncdata/meta.db'
 
-df = animals_by_genotype(db_path, ['dawt','datg'])
-df = append_external_identifiers(db_path, df)
-df = df.drop(['Animal_id','UZH/iRATS','Genotype_id'], axis=1)
+df = animals_by_genotype(db_path, ['datg','dawt'])
+df = df.rename(columns={'Animal_id':'subject'})
+df['subject'] = df['subject'].apply(lambda x: animal_id('~/syncdata/meta.db','ETH/AIC',str(x),reverse=True))
+df = df.drop(['Genotype_id'], axis=1)
+df = df.loc[df['subject']!='FailedIDQuery']
 df.to_csv('../data/groups.csv')
