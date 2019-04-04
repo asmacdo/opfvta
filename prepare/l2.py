e0729126abc7955256134ab98d8662a508ac523b
Horea Christian <horea.christ@yandex.com>
2019-04-04 03:27:43 +0200

    Coordinate cluster filtering and no tonic filter
    as it is already included in the new BIDS generation script

--- prepare/l2.py
+++ prepare/l2.py
@@ -10,9 +10,15 @@ l1_base = '{}/l1/'.format(scratch_dir)
 groups_path = path.abspath('../data/groups.csv')
 groups = pd.read_csv(groups_path)
 
+coordinates_path = path.abspath('../data/implant_coordinates.csv')
+coordinates = pd.read_csv(coordinates_path)
+coordinates = coordinates.loc[coordinates['Best Cluster']==True]
+pas = coordinates['PA rel. Bregma [mm]'].tolist()
+depths = coordinates['Depth rel. skull [mm]'].tolist()
+
 filtered_groups = groups.loc[
-	(groups['Depth rel. skull [mm]'] >= 4.4) &
-	(groups['PA rel. Bregma [mm]'] >= -3.3)
+	(groups['Depth rel. skull [mm]'].isin(depths)) &
+	(groups['PA rel. Bregma [mm]'].isin(pas))
 	]
 filtered_animals = [str(i) for i in filtered_groups['Subject'].tolist()]
 other_animals = [str(i) for i in groups.loc[~groups['Subject'].isin(filtered_animals), 'Subject'].tolist()]
@@ -22,8 +28,6 @@ glm.l2_common_effect(l1_base,
 	mask='{}usr/share/mouse-brain-atlases/dsurqec_200micron_mask.nii'.format(prefix),
 	groupby='task',
 	n_jobs_percentage=.33,
-	#The JPogT task in the current data has only one run, and cannot be modelled at the second level.
-	exclude={'task':['JPogT'],},
 	out_base=scratch_dir,
 	)
 
@@ -32,12 +36,10 @@ glm.l2_common_effect(l1_base,
 	mask='{}usr/share/mouse-brain-atlases/dsurqec_200micron_mask.nii'.format(prefix),
 	groupby='subject_set',
 	n_jobs_percentage=.33,
-	#Only considering block stimulation (filtering out tonic and phasic stimulation).
-	exclude={'task':['JPogT','JPogP','CogP'],},
+	exclude={'task':['JPogP','CogP'],},
 	out_base=scratch_dir,
 	target_set=[
 		{'subject':filtered_animals},
 		{'subject':other_animals},
 		],
 	)
-
