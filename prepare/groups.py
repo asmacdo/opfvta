2a1bab620213dc73e1532fd3fa324b7b98571dc5
Horea Christian <horea.christ@yandex.com>
2019-04-07 23:47:26 +0200

    Implant and genotype group selection

--- prepare/groups.py
+++ prepare/groups.py
@@ -1,11 +1,57 @@
 from labbookdb.report.tracking import append_external_identifiers
-from labbookdb.report.selection import animal_id, animals_by_genotype
+from labbookdb.report.selection import animal_id, animals_by_genotype, animal_operations, parameterized
+import pandas as pd
+from os import path
+import numpy as np
 
 db_path='~/syncdata/meta.db'
 
 df = animals_by_genotype(db_path, ['datg','dawt'])
-df = df.rename(columns={'Animal_id':'subject'})
-df['subject'] = df['subject'].apply(lambda x: animal_id('~/syncdata/meta.db','ETH/AIC',str(x),reverse=True))
+ids = [str(i) for i in df['Animal_id'].unique()]
+df = df.rename(columns={'Animal_id':'Subject'})
+df['Subject'] = df['Subject'].apply(lambda x: animal_id('~/syncdata/meta.db','ETH/AIC',str(x),reverse=True))
 df = df.drop(['Genotype_id'], axis=1)
-df = df.loc[df['subject']!='FailedIDQuery']
+
+_operations = animal_operations(db_path, animal_ids=ids)
+operations = pd.DataFrame([])
+for i in _operations['Animal_id'].unique():
+	df_ = _operations.loc[_operations['Animal_id']==i]
+	df_ = df_.drop(['Operation_id','Operation_operator_id','Operation_anesthesia_id'], 1)
+	if len(df_) == 1:
+		df_['Operation_delay'] = 0
+		operations = pd.concat([operations,df_],sort=True)
+	else:
+		a,b = df_['Operation_date'].unique()
+		delay = (b-a)/np.timedelta64(1, 'D')
+		delay = round(int(delay))
+		df_ = df_.groupby('Animal_id').transform(lambda x: x.bfill().ffill())
+		df_['Animal_id'] = i
+		df_['Operation_date'] = df_['Operation_date'].tolist()[0]
+		df_ = df_.drop_duplicates()
+		df_['Operation_delay'] = delay
+		operations = pd.concat([operations,df_],sort=True)
+
+operations['PA rel. Bregma [mm]'] = operations['OrthogonalStereotacticTarget_posteroanterior']
+operations['Depth rel. skull [mm]'] = operations['OrthogonalStereotacticTarget_depth']
+operations['\Delta PA_{Bregma}'] = operations['OrthogonalStereotacticTarget_posteroanterior']
+# Removing internal LabbookDB identifier columns
+operations = operations.drop(
+	[
+		'Operation_animal_id',
+		'OpticFiberImplantProtocol_code',
+		'OpticFiberImplantProtocol_id',
+		'OpticFiberImplantProtocol_name',
+		'OpticFiberImplantProtocol_optic_fiber_implant_id',
+		'OpticFiberImplantProtocol_stereotactic_target_id',
+	],
+	axis=1,
+	)
+
+operations = operations.rename(columns={'Animal_id':'Subject'})
+operations['Subject'] = operations['Subject'].apply(lambda x: animal_id(db_path, 'ETH/AIC', x, reverse=True))
+df = pd.merge(df, operations, on='Subject', how='inner')
+df = df.loc[df['Subject']!='FailedIDQuery']
+# Remove DR implanted animals
+df = df.loc[df['OrthogonalStereotacticTarget_reference']!='lambda']
+df = df.loc[df['OrthogonalStereotacticTarget_depth'].notnull()]
 df.to_csv('../data/groups.csv')
