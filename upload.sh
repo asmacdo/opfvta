695d8512c97e24141dc6dd114da9434b3bc492a7
Horea Christian <horea.christ@gmail.com>
2019-04-28 00:21:39 +0200

    special upload from bartholin to darkhost

--- upload.sh
+++ upload.sh
@@ -1,1 +1,2 @@
-rsync -avP article.pdf dreamhost:chymera.eu/articles/opfvta.pdf
+rsync -avP article.pdf dreamhost:chymera.eu/articles/opfvta.pdf ||\
+	rsync -avP article.pdf darkhost:bartholin_opfvta.pdf
