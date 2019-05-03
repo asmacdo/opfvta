dd4b05d45aaf8c999fff41abb98599d4754ad110
Horea Christian <horea.christ@yandex.com>
2019-05-04 01:35:10 +0200

    Use standard dgamma functions

--- prepare/l1.py
+++ prepare/l1.py
@@ -6,7 +6,7 @@ scratch_dir = '~/.scratch/opfvta'
 preprocess_base = path.join(scratch_dir,'preprocess')
 
 glm.l1(preprocess_base,
-	bf_path='../data/chr_beta1.txt',
+	#bf_path='../data/chr_beta1.txt',
 	habituation='in_main_contrast',
 	mask='mouse',
 	keep_work=False,
