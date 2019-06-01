0ca47e01b620c95b344a2cdaebfc5af7b86b6f7c
Horea Christian <horea.christ@yandex.com>
2019-06-02 00:39:26 +0200

    Supporting either cbv and bold

--- prepare/seed_analysis.py
+++ prepare/seed_analysis.py
@@ -7,12 +7,13 @@ scratch_dir = path.join(scratch_dir,'opfvta')
 
 template , substitutions = bids_autofind('{}/preprocess/'.format(scratch_dir),
         path_template='sub-{{subject}}/ses-{{session}}/func/'\
-                'sub-{{subject}}_ses-{{session}}_task-{{task}}_acq-{{acquisition}}_run-{{run}}_cbv.nii.gz',
+                'sub-{{subject}}_ses-{{session}}_task-{{task}}_acq-{{acquisition}}_run-{{run}}_{{modality}}.nii.gz',
         match_regex='.+sub-(?P<sub>.+)/ses-(?P<ses>.+)/func/'\
-                '.*?_task-(?P<task>.+)_acq-(?P<acquisition>.+)_run-(?P<run>.+)_cbv\.nii\.gz',
+                '.*?_task-(?P<task>.+)_acq-(?P<acquisition>.+)_run-(?P<run>.+)_(?P<modality>cbv|bold)\.nii\.gz',
         )
 
-fc_results = fc.seed_based(substitutions, '/usr/share/mouse-brain-atlases/dsurqec_200micron_roi-dr.nii', '/usr/share/mouse-brain-atlases/dsurqec_200micron_mask.nii',
+fc_results = fc.seed_based(substitutions, '../data/vta_right.nii.gz', '/usr/share/mouse-brain-atlases/dsurqec_200micron_mask.nii',
 	ts_file_template=template,
-	save_results='{}/fc/sub-{{subject}}/ses-{{session}}/sub-{{subject}}_ses-{{session}}_task-{{task}}_acq-{{acquisition}}_run-{{run}}_cbv.nii.gz'.format(scratch_dir),
-	)
+	save_results='{}/fc/sub-{{subject}}/ses-{{session}}/sub-{{subject}}_ses-{{session}}_task-{{task}}_acq-{{acquisition}}_run-{{run}}_{{modality}}.nii.gz'.format(scratch_dir),
+	cachedir='/mnt/overflow',
+        )
