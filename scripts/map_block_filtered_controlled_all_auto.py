4267957faaa1401c0eeebf57f819482a1ce7fd1f
Horea Christian <horea.christ@yandex.com>
2019-06-03 18:57:44 +0200

    Added new map files

---
+++ scripts/map_block_filtered_controlled_all_auto.py
@@ -0,0 +1,14 @@
+import samri.plotting.maps as maps
+
+scratch_dir = '~/.scratch/opfvta/'
+
+stat_map = "data/l2/alias-block_filtered_controlled/acq-EPI_tstat.nii.gz".format(scratch_dir)
+template = "/usr/share/mouse-brain-atlases/dsurqec_40micron_masked.nii"
+
+maps.stat3D(stat_map,
+	scale=0.3,
+	template=template,
+	show_plot=False,
+	threshold=3,
+	threshold_mesh=3,
+	)
