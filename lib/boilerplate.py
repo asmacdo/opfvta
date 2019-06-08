511642c3f86b355febc030fde97421eb641c3dbe
Horea Christian <horea.christ@yandex.com>
2019-06-08 07:30:41 +0200

    Updated boilerplate statistics

--- lib/boilerplate.py
+++ lib/boilerplate.py
@@ -9,7 +9,7 @@ def events_tab(events_path):
 	df = pd.read_csv(events_path,sep='\t')
 	df = df[['onset','duration','frequency','pulse_width']]
 	df = df.rename(columns={
-		'onset'         :   '\makecell[r]{Onset \\\\ {[}s{]}}',
+		'onset'	 :   '\makecell[r]{Onset \\\\ {[}s{]}}',
 		'duration'      :   '\makecell[r]{Duration \\\\ {[}s{]}}',
 		'frequency'     :   '\makecell[r]{Frequency \\\\ {[}Hz{]}}',
 		'pulse_width'   :   '\makecell[r]{Pulse Width \\\\ {[}s{]}}',
@@ -18,6 +18,20 @@ def events_tab(events_path):
 	df = df.reset_index(drop=True)
 	df_tex = df.to_latex(index=False, escape=False)
 	return df_tex
+
+def lateral(data_path,
+	):
+	from samri.report.roi import atlasassignment
+	from scipy.stats import ttest_rel
+	value_label='t Values'
+	df = atlasassignment(data_path,
+		lateralized=True,
+		value_label=value_label,
+		)
+	df[value_label] = df[value_label].apply(lambda x: np.mean([float(i) for i in x.split(', ')]))
+	p = ttest_rel(df.loc[df['side']=='left',value_label], df.loc[df['side']=='right',value_label]).pvalue
+	p = float_to_tex(p)
+	return p
 
 def posthoc_t(comparison,
 	data_path='data/functional_t.csv',
@@ -105,9 +119,7 @@ def anova(
 
 	formula = '{} ~ {}'.format(dependent_variable, expression)
 	ols = smf.ols(formula, df).fit()
-	print(ols)
 	summary = sm.stats.anova_lm(ols, typ=typ, robust='hc3')
-	print(summary)
 	tex = inline_anova(summary, factor, 'tex', **kwargs)
 	return tex
 
@@ -176,7 +188,6 @@ def corecomparison_factorci(factor,
 	model = smf.mixedlm(formula, df, groups='Uid')
 	fit = model.fit()
 	summary = fit.summary()
-	#print(summary)
 	tex = inline_factor(summary, factor, 'tex', **kwargs)
 	return tex
 
@@ -250,63 +261,63 @@ def variancep(
 	return float_to_tex(result.pvalue, max_len, **kwargs)
 
 def variance_test(
-        factor,
-        workflow,
-        metric,
-        df_path='data/variance_data.csv',
-        template=False,
-        max_len=2,
-        **kwargs
-        ):
-        df = pd.read_csv(path.abspath(df_path))
-        df = df.loc[df['Processing']==workflow]
-        #contrast
-        df = df.loc[df['acquisition'].str.contains('cbv')]
-        model= metric + '~ C(Subject) + C(Session)'
-        ols = smf.ols(model, df).fit()
-        anova = sm.stats.anova_lm(ols, typ=3, robust='hc3')
-        tex = inline_anova(anova,  factor, 'tex', **kwargs)
-        return tex
+	factor,
+	workflow,
+	metric,
+	df_path='data/variance_data.csv',
+	template=False,
+	max_len=2,
+	**kwargs
+	):
+	df = pd.read_csv(path.abspath(df_path))
+	df = df.loc[df['Processing']==workflow]
+	#contrast
+	df = df.loc[df['acquisition'].str.contains('cbv')]
+	model= metric + '~ C(Subject) + C(Session)'
+	ols = smf.ols(model, df).fit()
+	anova = sm.stats.anova_lm(ols, typ=3, robust='hc3')
+	tex = inline_anova(anova, factor, 'tex', **kwargs)
+	return tex
 
 def fstatistic_smoothness(factor,
-        df_path='data/smoothness_data.csv',
-        dependent_variable='Smoothness Change Factor',
-        expression='Processing*Template',
-        exclusion_criteria={},
-        **kwargs
-        ):
-        df_path = path.abspath(df_path)
-        df = pd.read_csv(df_path)
+	df_path='data/smoothness_data.csv',
+	dependent_variable='Smoothness Change Factor',
+	expression='Processing*Template',
+	exclusion_criteria={},
+	**kwargs
+	):
+	df_path = path.abspath(df_path)
+	df = pd.read_csv(df_path)
 
-        df = df.loc[df['Processing']!='Unprocessed']
+	df = df.loc[df['Processing']!='Unprocessed']
 
-        for key in exclusion_criteria.keys():
-                df = df.loc[~df[key].isin(exclusion_criteria[key])]
+	for key in exclusion_criteria.keys():
+		df = df.loc[~df[key].isin(exclusion_criteria[key])]
 
-        formula='Q("{}") ~ {}'.format(dependent_variable, expression)
-        ols = smf.ols(formula, df).fit()
-        anova = sm.stats.anova_lm(ols, typ=2)
-        tex = inline_anova(anova, factor, 'tex', **kwargs)
-        return tex
+	formula='Q("{}") ~ {}'.format(dependent_variable, expression)
+	ols = smf.ols(formula, df).fit()
+	anova = sm.stats.anova_lm(ols, typ=2)
+	tex = inline_anova(anova, factor, 'tex', **kwargs)
+	return tex
 
 def meanVar(ret,
-        df_path='data/smoothness_data.csv',
-        factor = 'Smoothness Change Factor',
-        Processing = 'Legacy',
-        template=False,
-        max_len=2,
-        **kwargs
-        ):
-        df_path = path.abspath(df_path)
-        df = pd.read_csv(df_path)
+	df_path='data/smoothness_data.csv',
+	factor = 'Smoothness Change Factor',
+	Processing = 'Legacy',
+	template=False,
+	max_len=2,
+	**kwargs
+	):
+	df_path = path.abspath(df_path)
+	df = pd.read_csv(df_path)
 
-        df = df.loc[df['Processing']!='Unprocessed']
-        data = df.loc[df['Processing']==Processing, factor]
+	df = df.loc[df['Processing']!='Unprocessed']
+	data = df.loc[df['Processing']==Processing, factor]
 
-        if(ret == 'mean'):
-                result = data.mean()
-        if(ret == 'sd'):
-                result = data.std()
+	if(ret == 'mean'):
+		result = data.mean()
+	if(ret == 'sd'):
+		result = data.std()
 
-        return float_to_tex(result, max_len, **kwargs)
+	return float_to_tex(result, max_len, **kwargs)
 
