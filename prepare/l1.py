5b38dc605e7cc43fda77d463902c0e2507514cd5
Horea Christian <horea.christ@gmail.com>
2019-09-17 13:37:56 +0200

    Uncommented first level

--- prepare/l1.py
+++ prepare/l1.py
@@ -5,19 +5,19 @@ scratch_dir = '~/.scratch/opfvta'
 
 preprocess_base = path.join(scratch_dir,'preprocess')
 
-#glm.l1(preprocess_base,
-#	bf_path='../data/chr_beta1.txt',
-#	habituation=None,
-#	mask='mouse',
-#	n_jobs_percentage=.33,
-#	exclude={
-#		'task':['rest','JPogT'],
-#		},
-#	match={'type':['cbv']},
-#	invert=True,
-#	workflow_name='l1',
-#	out_base=scratch_dir,
-#	)
+glm.l1(preprocess_base,
+	bf_path='../data/chr_beta1.txt',
+	habituation=None,
+	mask='mouse',
+	n_jobs_percentage=.33,
+	exclude={
+		'task':['rest','JPogT'],
+		},
+	match={'type':['cbv']},
+	invert=True,
+	workflow_name='l1',
+	out_base=scratch_dir,
+	)
 
 glm.seed(preprocess_base,'../data/vta_right.nii.gz',
 	mask='mouse',
