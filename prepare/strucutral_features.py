ae8c81b1072fe7a21e6af1bdee2f5c9c90ab3d90
Horea Christian <horea.christ@yandex.com>
2019-09-27 20:57:26 +0200

    Renamed analysis routine

--- prepare/strucutral_features.py
+++ prepare/strucutral_features.py
@@ -2,17 +2,16 @@ import os
 import pandas as pd
 from samri.fetch.local import prepare_feature_map
 
-df = pd.read_csv('../data/structural_connectivity.csv')
+df = pd.read_csv('../data/structural_features.csv')
 
 region_data_template = '/usr/share/ABI-connectivity-data/Ventral_tegmental_area-{}/'
-target_path_template = '~/.scratch/opfvta/structural_connectivity/vta_{}.nii.gz'
+target_path_template = '~/.scratch/opfvta/structural_features/sub-{}/ses-1/anat/vta.nii.gz'
 
 region_data_template = os.path.abspath(os.path.expanduser(region_data_template))
 target_path_template = os.path.abspath(os.path.expanduser(target_path_template))
-if not os.path.exists(target_path_template):
-	    os.makedirs(target_path_template)
 for ix, row in df.iterrows():
 	identifier = row['identifier']
+	print(identifier)
 	invert = row['laterality'] == 'right'
 	prepare_feature_map(region_data_template.format(identifier),
 		invert_lr=invert,
