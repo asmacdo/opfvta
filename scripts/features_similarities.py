bb8cc3d9794dfa3dad524d868e8aaf306fd5acc6
Horea Christian <horea.christ@yandex.com>
2019-10-25 10:02:39 +0200

    Updated ANTs similarity metric calculation

--- scripts/features_similarities.py
+++ scripts/features_similarities.py
@@ -1,4 +1,26 @@
 from samri.report.registration import measure_sim
 
-mi = measure_sim('data/l2/alias-block_filtered_controlled/acq-EPI_tstat.nii.gz','data/vta_projection_tstat.nii.gz')
-print(mi)
+cc = measure_sim('data/l2/alias-block_filtered_controlled/acq-EPI_tstat.nii.gz','data/vta_projection_tstat.nii.gz',
+        sampling_percentage=1.0,
+        metric='CC',
+        mask='/usr/share/mouse-brain-atlases/dsurqec_200micron_mask.nii',
+        )
+gc = measure_sim('data/l2/alias-block_filtered_controlled/acq-EPI_tstat.nii.gz','data/vta_projection_tstat.nii.gz',
+        sampling_percentage=1.0,
+        metric='GC',
+        mask='/usr/share/mouse-brain-atlases/dsurqec_200micron_mask.nii',
+        )
+mi = measure_sim('data/l2/alias-block_filtered_controlled/acq-EPI_tstat.nii.gz','data/vta_projection_tstat.nii.gz',
+        radius_or_number_of_bins=32,
+        sampling_percentage=1.0,
+        metric='MI',
+        mask='/usr/share/mouse-brain-atlases/dsurqec_200micron_mask.nii',
+        )
+
+out_str = 'CC={cc}, GC={gc}, MI={mi}'.format(
+        cc=cc['similarity'],
+        gc=gc['similarity'],
+        mi=mi['similarity'],
+        )
+
+print(out_str)
