8b411e1cf005872246d15a67dde039b6eaf7279c
Horea Christian <horea.christ@yandex.com>
2019-10-31 02:03:53 +0100

    Added posteroanterior coordinate separation plot

---
+++ scripts/implant_coordinates_pa.py
@@ -0,0 +1,46 @@
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import pandas as pd
+from os import path
+import seaborn as sns
+
+data_path = path.abspath('data/implant_coordinates.csv')
+df = pd.read_csv(data_path)
+lines_markersize = mpl.rcParams['lines.markersize']
+
+df = df.rename(columns={
+	'VTA t': 'VTA t ',
+	'Count': 'Count ',
+	})
+
+cmap = sns.cubehelix_palette(dark=.3, light=.8, as_cmap=True)
+ax = sns.scatterplot(
+	x='PA rel. Bregma [mm]',
+	y='Depth rel. skull [mm]',
+	hue='VTA t ',
+	size='Count ',
+	data=df,
+	sizes=(20, 100),
+	palette=cmap,
+	)
+best_coordinates = df.loc[df['PA rel. Bregma [mm]']>=-3.25]
+plt.scatter(
+	best_coordinates['PA rel. Bregma [mm]'].tolist(),
+	best_coordinates['Depth rel. skull [mm]'].tolist(),
+	c='w',
+	s=lines_markersize/8.,
+	)
+# Hack to circumvent strange decimal numbering
+handles, labels = ax.get_legend_handles_labels()
+newlabels = []
+for i in labels:
+	try:
+		i = float(i)
+	except:
+		pass
+	else:
+		i = '{0:.1f}'.format(i)
+		i = str(i)
+	newlabels.append(i)
+ax.legend(handles, newlabels)
+plt.gca().invert_yaxis()
