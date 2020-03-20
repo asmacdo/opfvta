5f221060001649e039bfb9de394460b6dbbaaf75
Horea Christian <horea.christ@gmail.com>
2020-03-20 06:39:45 +0100

    Added supplementary figure for qualitative comparison

---
+++ scripts/features_filtered_coronal_contour.py
@@ -0,0 +1,14 @@
+import samri.plotting.maps as maps
+
+heatmap_image = 'data/l2/alias-block_filtered_controlled/acq-EPI_tstat.nii.gz'
+contour_image = 'data/vta_projection_tstat.nii.gz'
+
+maps.slices(heatmap_image,
+	contour_image=contour_image,
+	ratio=7/5.,
+	auto_figsize=False,
+	style=False,
+	position_vspace=0,
+	positive_only=True,
+	slice_spacing=0.45,
+	)
