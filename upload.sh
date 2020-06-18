6ca5bc996e46c818f6877b6074d99310720ee17a
Horea Christian <chr@chymera.eu>
2020-06-18 01:57:14 -0400

    No longer uploading publisher-specific version

--- upload.sh
+++ upload.sh
@@ -1,3 +1,2 @@
 [[ ! -z "$HOSTNAME" ]] && HOSTNAME="${HOSTNAME}_"
 rsync -avP article.pdf dreamhost:chymera.eu/articles/${HOSTNAME}opfvta.pdf
-rsync -avP article_mp.pdf dreamhost:chymera.eu/articles/${HOSTNAME}opfvta_mp.pdf
