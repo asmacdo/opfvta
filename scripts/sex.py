b37bf3e35d60f3d1ddc649e316d76257c89bef9e
Horea Christian <chr@chymera.eu>
2020-07-25 01:18:42 -0400

    BIDS-distribution robust metadata sourcing

--- scripts/sex.py
+++ scripts/sex.py
@@ -1,8 +1,12 @@
 import pandas as pd
 from os import path
 
-data_path = path.abspath('/usr/share/opfvta_bidsdata/participants.tsv')
-df = pd.read_csv(data_path, sep='\t')
+data_path = path.abspath(path.expanduser('~/.scratch/opfvta/bids/participants.tsv'))
+try:
+	df = pd.read_csv(data_path, sep='\t')
+except FileNotFoundError:
+	data_path = '/usr/share/opfvta_bidsdata/participants.tsv'
+	df = pd.read_csv(data_path, sep='\t')
 
 m = len(df.loc[(df['sex']=='m')])
 f = len(df.loc[(df['sex']=='f')])
