009493ca2499415c95301f525e0408641db7c566
Horea Christian <chr@chymera.eu>
2020-09-03 06:07:37 -0400

    Renamed ISMRM slides to pitch

--- compile.sh
+++ compile.sh
@@ -4,8 +4,17 @@ TARGET="${1}"
 WHITELIST="
 	article.tex
 	poster.tex
+	pitch.tex
 	slides.tex
 	"
+
+declare -A BIBER_LIST;
+BIBER_LIST["pitch"]=1;
+BIBER_LIST["slides"]=1;
+#if (('list[$myvar]')); then echo "$myvar is in the list"; fi
+
+echo ${BIBER_LIST[@]}
+echo ${BIBER_LIST["$TARGET"]}
 
 if [[ "$TARGET" = "all" ]] || [[ "$TARGET" = "" ]]; then
 	for ITER_TARGET in *.tex; do
@@ -18,7 +27,7 @@ else
 	pdflatex -shell-escape "${TARGET}.tex" || { echo "Initial pdflatex failed"; exit $ERRCODE; }
 	pythontex.py ${TARGET}.tex || exit 1
 	pdflatex -shell-escape "${TARGET}.tex" || { echo "Post-PythonTeX pdflatex failed"; exit $ERRCODE; }
-	if [[ "${TARGET}" = "slides" ]]; then
+	if ((${BIBER_LIST["$TARGET"]})); then
 		biber "${TARGET}" || { echo "Biber failed"; exit $ERRCODE; }
 	else
 		bibtex "${TARGET}" || { echo "Bibtex failed"; exit $ERRCODE; }
