59bf3e809256d7227468f584fa37ba40ff4efcf1
Horea Christian <chr@chymera.eu>
2020-10-29 04:45:18 -0400

    Debugging animal numbers

--- scripts/animals_wt.py
+++ scripts/animals_wt.py
@@ -1,6 +1,13 @@
 import pandas as pd
 from os import path
 
+ddata_path = path.abspath(path.expanduser('~/.scratch/opfvta/bids/participants.tsv'))
+try:
+        ddf = pd.read_csv(ddata_path, sep='\t')
+except FileNotFoundError:
+        ddata_path = '/usr/share/opfvta_bidsdata/participants.tsv'
+        ddf = pd.read_csv(ddata_path, sep='\t')
+
 data_path = path.abspath('data/functional_t.csv')
 df = pd.read_csv(data_path)
 
