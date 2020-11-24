5a27fbbe2c37d79ce85fbd1f68c6d5a841f8c18f
Horea Christian <chr@chymera.eu>
2020-11-24 04:56:28 -0500

    Not returning double names

--- scripts/sex.py
+++ scripts/sex.py
@@ -8,6 +8,6 @@ except FileNotFoundError:
 	data_path = '/usr/share/opfvta_bidsdata/participants.tsv'
 	df = pd.read_csv(data_path, sep='\t')
 
-m = len(df.loc[(df['sex']=='m')])
-f = len(df.loc[(df['sex']=='f')])
+m = len(df.loc[(df['sex']=='m', 'participant_id')].unique())
+f = len(df.loc[(df['sex']=='f', 'participant_id')].unique())
 print('{} males and {} females'.format(m,f))
