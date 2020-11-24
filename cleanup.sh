c31100c02b40a50c13c4890db80ce6ea0cf554cf
Horea Christian <chr@chymera.eu>
2020-11-24 03:18:56 -0500

    cleaning up trace bibliography files

--- cleanup.sh
+++ cleanup.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-rm -rf auto_fig_py*.{png,pdf} *.aux *.bbl *.blg *.log _minted-slides *.nav *.out *.pgf pythontex-files-* *.pytxcode *.snm *.toc *.vrb
+rm -rf auto_fig_py*.{png,pdf} *.aux *.bbl *.bcf *.blg *.log _minted-slides *.nav *.out *.pgf pythontex-files-* *.pytxcode *.snm *.toc *.vrb
