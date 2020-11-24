cc598cfa5ff477f83d82f3b0ec9cadae72b3db6d
Horea Christian <chr@chymera.eu>
2020-11-24 04:58:09 -0500

    Updated groups metadata generation

--- prepare/preprocess.py
+++ prepare/preprocess.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from os import path
 
 from samri.pipelines.preprocess import generic, legacy
@@ -6,10 +7,16 @@ from samri.pipelines import manipulations
 scratch_dir = '~/.scratch/opfvta'
 bids_base = path.join(scratch_dir,'bids')
 
+groups_path = path.abspath('../data/groups.csv')
+groups = pd.read_csv(groups_path)
+groups = groups.loc[groups['OrthogonalStereotacticTarget_depth'].notnull()]
+subjects = groups.Subject.unique()
+
 # Preprocess all of the data:
 generic(bids_base,
 	functional_blur_xy=0.25,
 	template='mouse',
+	subjects=subjects,
 	functional_match={'acquisition':['EPI'],},
 	structural_match={'acquisition':['TurboRARE'],},
         workflow_name='preprocess',
