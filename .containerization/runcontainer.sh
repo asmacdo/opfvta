135dcc5d262ae7e2dbf9215c583cc1c3d772581f
Horea Christian <chr@chymera.eu>
2023-04-05 16:57:52 -0400

    Added containerization script and recipes

---
+++ .containerization/runcontainer.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+if [ ! -d ~/.local/share/data/opfvta_bidsdata ]; then
+	mkdir -p ~/.local/share/data/opfvta_bidsdata
+	pushd ~/.local/share/data/opfvta_bidsdata
+	wget "https://zenodo.org/record/3575149/files/opfvta_bidsdata-2.0.tar.xz"
+	tar xvf "opfvta_bidsdata-2.0.tar.xz"
+	mv opfvta_bidsdata-2.0/* .
+	rm opfvta_bidsdata*
+fi
+
+pushd ../
+	podman build . \
+		-f Containerfile.partial \
+		-t opfvta-gentoo
+
+	# podman build . \
+	#       -f Containerfile \
+	#       -t opfvta-gentoo
+
+	podman run -it \
+		-v ~/.local/share/data/opfvta_bidsdata:/usr/share/opfvta_bidsdata:Z \
+		--detach \
+		opfvta-gentoo /bin/bash
+popd
