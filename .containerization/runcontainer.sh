43fc63f709e915fe8d7976adca874e7127e2f315
Horea Christian <chr@chymera.eu>
2023-04-06 15:02:45 -0400

    More atomized container scripts
    Co-authored-by: Austin Macdonald <austin@dartmouth.edu>

--- .containerization/runcontainer.sh
+++ .containerization/runcontainer.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# Downloading the data separately to bind mount into container
 if [ ! -d ~/.local/share/data/opfvta_bidsdata ]; then
       mkdir -p ~/.local/share/data/opfvta_bidsdata
       pushd ~/.local/share/data/opfvta_bidsdata
@@ -10,17 +11,17 @@ if [ ! -d ~/.local/share/data/opfvta_bidsdata ]; then
       popd
 fi
 
+# Directory so that analysis traces are available on the host for inspection
+mkdir -p ~/.local/share/data/opfvta_scratch
+
 pushd ..
-      #podman build . \
-      #	-f .containerization/Containerfile.partial \
-      #	-t opfvta-gentoo
-
-      podman build . \
-	    -f .containerization/Containerfile \
-	    -t opfvta-gentoo
-
-      podman run -it \
-	      -v ~/.local/share/data/opfvta_bidsdata:/usr/share/opfvta_bidsdata:Z \
-	      --detach \
-	      opfvta-gentoo /bin/bash
+       # Running container with host-based directories bind mounted
+       # Run an instance of the image
+       podman run -it \
+               -v ~/.local/share/data/opfvta_bidsdata:/usr/share/opfvta_bidsdata:Z \
+               -v ~/.local/share/data/opfvta_scratch:/root/.scratch/:Z \
+               --detach \
+               --name opfvta-gentoo-instance-1 \
+               opfvta-gentoo /bin/bash
 popd
+
