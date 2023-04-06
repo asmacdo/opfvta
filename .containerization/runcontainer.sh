b12e8d0e8a5b287f81e1f073ba7af3274f5bd1ac
Horea Christian <chr@chymera.eu>
2023-04-06 02:56:06 -0400

    Building full image

--- .containerization/runcontainer.sh
+++ .containerization/runcontainer.sh
@@ -9,15 +9,15 @@ if [ ! -d ~/.local/share/data/opfvta_bidsdata ]; then
 	rm opfvta_bidsdata*
 fi
 
-podman build . \
-	-f Containerfile.partial \
-	--name opfvta \
-	-t opfvta-gentoo
-
-# podman build . \
-#       -f Containerfile \
+#podman build . \
+#	-f Containerfile.partial \
 #	--name opfvta \
-#       -t opfvta-gentoo
+#	-t opfvta-gentoo
+
+podman build . \
+      -f Containerfile \
+      --name opfvta \
+      -t opfvta-gentoo
 
 podman run -it \
 	-v ~/.local/share/data/opfvta_bidsdata:/usr/share/opfvta_bidsdata:Z \
