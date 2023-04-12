f63c887c6e053b506442c1eff0a8787913b8d17c
Horea Christian <chr@chymera.eu>
2023-04-12 03:19:01 -0400

    Dropping deprecated upload script

--- upload.sh
+++
@@ -1,7 +0,0 @@
-SERVER='dreamarticles'
-WEBSITE='articles.chymera.eu'
-NAME=$(basename $(pwd))
-[[ ! -z "$HOSTNAME" ]] && HOSTNAME="${HOSTNAME}_"
-
-rsync -avP ${1}.pdf ${SERVER}:${WEBSITE}/${HOSTNAME}${NAME}_${1}.pdf &&\
-	                echo "Article uploaded to http://${WEBSITE}/${HOSTNAME}${NAME}_${1}.pdf"
